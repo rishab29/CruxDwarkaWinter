a54951f805ab1e585e40792fa77d4f30178d05f6
rishab29 <rishab.kapoor.29@gmail.com>
2018-12-15 22:33:32 +0530

    Updated Code Lecture1

--- src/Lecture1/HelloWorld.java
+++ src/Lecture1/HelloWorld.java
@@ -3,6 +3,8 @@ package Lecture1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		
+		//Printing on console
 		System.out.print("Hello World");
 		System.out.println("Hello World");
 		System.out.print("Hello World");
