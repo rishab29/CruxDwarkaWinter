0fa7884b4d5ae82dc6891cead82faab8d2f58c72
rishab29 <rishab.kapoor.29@gmail.com>
2019-03-09 18:43:17 +0530

    Graph part 1

--- src/Lecture22/Graphs.java
+++ src/Lecture22/Graphs.java
@@ -2,6 +2,7 @@ package Lecture22;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Set;
 
 public class Graphs {
@@ -107,21 +108,130 @@ public class Graphs {
 			vtx.display();
 		}
 	}
-	
-	public boolean hasPath(String name1,String name2) {
-		Vertex vtx1=this.vtces.get(name1);
-		Vertex vtx2=this.vtces.get(name2);
-		
-		if(vtx1==null||vtx2==null) {
+
+	public boolean hasPath(String name1, String name2) {
+		Vertex vtx1 = this.vtces.get(name1);
+		Vertex vtx2 = this.vtces.get(name2);
+
+		if (vtx1 == null || vtx2 == null) {
 			return false;
 		}
-		HashMap<Vertex,Boolean> explored=new HashMap<>();
-		this.hasPath(vtx1,vtx2,explored);
+		HashMap<Vertex, Boolean> explored = new HashMap<>();
+		return this.hasPathDFSI(vtx1, vtx2, explored);
 	}
-	
-	private boolean hasPath(Vertex vtx1,Vertex vtx2,HashMap<Vertex, Boolean> explored) {
-		
-		
+
+	private boolean hasPath(Vertex vtx1, Vertex vtx2, HashMap<Vertex, Boolean> explored) {
+		if (!explored.containsKey(vtx1)) {
+			explored.put(vtx1, true);
+		} else {
+			return false;
+		}
+
+		if (vtx1.nbrs.containsKey(vtx2)) {
+			return true;
+		}
+
+		Set<Vertex> nbrs = vtx1.nbrs.keySet();
+		for (Vertex nbr : nbrs) {
+			if (this.hasPath(nbr, vtx2, explored)) {
+				return true;
+			}
+		}
+
+		return false;
+
+	}
+
+	private boolean hasPathBFS(Vertex vtx1, Vertex vtx2, HashMap<Vertex, Boolean> explored) {
+		LinkedList<Vertex> queue = new LinkedList<>();
+		queue.add(vtx1);
+		while (!queue.isEmpty()) {
+			Vertex rv = queue.remove();
+			if (!explored.containsKey(rv)) {
+				explored.put(rv, true);
+				if (rv.nbrs.containsKey(vtx2)) {
+					return true;
+				} else {
+					Set<Vertex> nbrs = rv.nbrs.keySet();
+					for (Vertex nbr : nbrs) {
+						if (!explored.containsKey(nbr)) {
+							queue.add(nbr);
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean hasPathDFSI(Vertex vtx1, Vertex vtx2, HashMap<Vertex, Boolean> explored) {
+		LinkedList<Vertex> stack = new LinkedList<>();
+		stack.addFirst(vtx1);
+		while (!stack.isEmpty()) {
+			Vertex rv = stack.removeFirst();
+			if (!explored.containsKey(rv)) {
+				explored.put(rv, true);
+				if (rv.nbrs.containsKey(vtx2)) {
+					return true;
+				} else {
+					Set<Vertex> nbrs = rv.nbrs.keySet();
+					for (Vertex nbr : nbrs) {
+						if (!explored.containsKey(nbr)) {
+							stack.addFirst(nbr);
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	public void bft() {
+		LinkedList<Vertex> queue = new LinkedList<>();
+		HashMap<Vertex, Boolean> explored = new HashMap<>();
+		Collection<Vertex> vtces = this.vtces.values();
+		for (Vertex vtx : vtces) {
+			if (!explored.containsKey(vtx)) {
+				queue.add(vtx);
+			}
+			while (!queue.isEmpty()) {
+				Vertex rv = queue.remove();
+				if (!explored.containsKey(rv)) {
+					explored.put(rv, true);
+					System.out.print(rv.name);
+					Set<Vertex> nbrs = rv.nbrs.keySet();
+					for (Vertex nbr : nbrs) {
+						if (!explored.containsKey(nbr)) {
+							queue.add(nbr);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public void dft() {
+		LinkedList<Vertex> stack = new LinkedList<>();
+		HashMap<Vertex, Boolean> explored = new HashMap<>();
+		Collection<Vertex> vtces = this.vtces.values();
+		for (Vertex vtx : vtces) {
+			if (!explored.containsKey(vtx)) {
+				stack.addFirst(vtx);
+			}
+			while (!stack.isEmpty()) {
+				Vertex rv = stack.removeFirst();
+				if (!explored.containsKey(rv)) {
+					explored.put(rv, true);
+					System.out.print(rv.name);
+					Set<Vertex> nbrs = rv.nbrs.keySet();
+					for (Vertex nbr : nbrs) {
+						if (!explored.containsKey(nbr)) {
+							stack.addFirst(nbr);
+						}
+					}
+				}
+			}
+		}
 	}
 
 }
--- src/Lecture22/client.java
+++ src/Lecture22/client.java
@@ -4,36 +4,37 @@ public class client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      Graphs graph=new Graphs();
-      
-      graph.addVertex("A");
-      graph.addVertex("B");
-      graph.addVertex("C");
-      graph.addVertex("D");
-      graph.addVertex("E");
-      graph.addVertex("F");
-      graph.addVertex("G");
-      
-      graph.addEdge("A", "B", 10);
-      graph.addEdge("A", "D", 40);
-      graph.addEdge("B", "C", 10);
-      graph.addEdge("C", "D", 10);
-      graph.addEdge("D", "E", 2);
-      graph.addEdge("E", "F", 8);
-      graph.addEdge("F", "G", 3);
-      graph.addEdge("E", "G", 3);
-      
-     // graph.removeEdge("D", "E");
-      graph.removeVertex("A");
-      System.out.println(graph.numEdges());
-      System.out.println(graph.numVertices());
-      
-      graph.display();
-      
-      
-      
-      
-      
+		Graphs graph = new Graphs();
+
+		graph.addVertex("A");
+		graph.addVertex("B");
+		graph.addVertex("C");
+		graph.addVertex("D");
+		graph.addVertex("E");
+		graph.addVertex("F");
+		graph.addVertex("G");
+
+		graph.addEdge("A", "B", 10);
+		graph.addEdge("A", "D", 40);
+		graph.addEdge("B", "C", 10);
+		graph.addEdge("C", "D", 10);
+		graph.addEdge("D", "E", 2);
+		graph.addEdge("E", "F", 8);
+		graph.addEdge("F", "G", 3);
+		graph.addEdge("E", "G", 3);
+
+	 graph.removeEdge("D", "E");
+		// graph.removeVertex("A");
+//		System.out.println(graph.numEdges());
+//		System.out.println(graph.numVertices());
+
+		graph.display();
+
+		System.out.println(graph.hasPath("A", "G"));
+		graph.bft();
+		System.out.println();
+		graph.dft();
+
 	}
 
 }
